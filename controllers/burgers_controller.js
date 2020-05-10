0c349ed561ca931c571c613940811fe6a5e6199b
Jaclyn Hardy <jackihardy@yahoo.com>
2020-05-10 10:01:40 -0700

    updated files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -14,6 +14,46 @@ router.get("/", function(req, res) {
     });
 });
 
+router.put("/api/burger", function (req, res) {
+    burger.create([
+        "meat", "veggie", "cheese", "bun"
+    ], [
+        req.body.meat, req.body.veggies, req.body.cheese, req.body.bun
+    ], function(result) {
+        res.json({ id: result.insertId });
+        console.log (req.body.meat);
+        console.log (req.body.veggies);
+        console.log (req.body.cheese);
+        console.log (req.body.bun);
+        
+    });
+});
+
+router.put("/api/burger/:id", function (req, res){
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        cheese: req.body.cheese
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status (404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+
+    burger.update({
+        bun: req.body.bun
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status (404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 
 module.exports = router; 
--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,26 @@
+var orm = require("../config/orm.js");
 
+var burger = {
+    all: function(cb) {
+        orm.all("burger", function(res) { 
+            cb(res);
+        });
+    } ,
+
+    create: function (cols, vals, cb) {
+        orm.create("burger", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    update: function(objColVlas, condition, cb) {
+        orm.update("burger", objColVlas, condition, function(res) {
+            cb(res);
+        });
+    }
+
+    //$("#delete").on(click);
+
+}
+
+module.exports = burger;
   
